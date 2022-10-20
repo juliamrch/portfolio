e3dcb11e736512db0cdb8328652006a7fbdffc7e
Julia March <contact@juliamarch.com>
2022-10-20 16:48:26 +0200

    added v to url

--- hugo.sh
+++ hugo.sh
@@ -1,3 +1,3 @@
-wget https://github.com/gohugoio/hugo/releases/download/hugo_extended_0.104.3_Linux-64bit.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v0.104.3/hugo_extended_0.104.3_Linux-64bit.tar.gz
 chmod +x hugo_extended_0.104.3_Linux-64bit.tar.gz
 ./hugo --gc --minify --destination public
