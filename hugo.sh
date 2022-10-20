01f647584a89d2635a4484ec6e4dd20c642d97e6
Julia March <contact@juliamarch.com>
2022-10-20 17:55:03 +0200

    fixed command

--- hugo.sh
+++ hugo.sh
@@ -1,3 +1,4 @@
 wget https://github.com/gohugoio/hugo/releases/download/v0.104.3/hugo_extended_0.104.3_Linux-64bit.tar.gz
-chmod +x hugo_extended_0.104.3_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.104.3_Linux-64bit.tar.gz
+chmod +x ./hugo
 ./hugo --gc --minify --destination public
