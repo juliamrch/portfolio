b56625a4404c50c271a9d78e8342545e7fdce029
Julia March <contact@juliamarch.com>
2022-10-20 15:48:58 +0200

    fixed hugo url

--- public/hugo.sh
+++ public/hugo.sh
@@ -1,4 +1,4 @@
-wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_v0.104.3_Linux-64bit.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v0.104.3/hugo_extended_0.104.3_Linux-64bit.tar.gz
 tar xvf hugo_v0.104.3_Linux-64bit.tar.gz
 chmod +x ./hugo
 ./hugo --gc --minify --destination public/
