1f9c8a3e4059ba7dc9abc95861431a73ac1500df
Julia March <contact@juliamarch.com>
2023-06-14 12:02:09 +0200

    add en var for hugo version

--- hugo.sh
+++ hugo.sh
@@ -1,4 +1,4 @@
-wget https://github.com/gohugoio/hugo/releases/download/v0.104.3/hugo_extended_0.104.3_Linux-64bit.tar.gz
-tar -xvf hugo_extended_0.104.3_Linux-64bit.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
+tar -xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 chmod +x ./hugo
 ./hugo --gc --minify --destination public
